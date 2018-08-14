1f443a2e21edf419f9e34a7e4d60357ac720ce84
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-14 06:44:04 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,25 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT projects.title AS project_title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY project_title ORDER BY project_title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name AS name, users.age AS age, SUM(pledges.amount) AS total_amount FROM users INNER JOIN pledges ON users.id = pledges.user_id GROUP BY name ORDER BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title AS Project, SUM(pledges.amount) - projects.funding_goal AS Amount FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY pledges.project_id HAVING SUM(pledges.amount) - projects.funding_goal >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name AS name, SUM(pledges.amount) AS total_amount FROM users INNER JOIN pledges ON users.id = pledges.user_id GROUP BY name ORDER BY total_amount "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category AS category, pledges.amount AS amount FROM projects INNER JOIN pledges ON projects.id = pledges.project_id where category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category AS category, SUM(pledges.amount) AS total_amount FROM projects INNER JOIN pledges ON pledges.project_id = projects.id GROUP BY category HAVING category = 'books'"
 end
