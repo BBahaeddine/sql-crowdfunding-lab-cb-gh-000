4bc361f7d07ef91ea864f1cdc28f12a7df7265c5
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-14 06:37:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category AS category, projects.name AS name, pledges.amount AS amount FROM projects INNER JOIN pledges GROUP BY category HAVING category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
