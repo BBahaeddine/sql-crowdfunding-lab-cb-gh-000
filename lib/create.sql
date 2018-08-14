bc0811a74de64eb4f0e18246390ad3965c77c2b5
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-14 05:25:48 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,5 @@
 CREATE TABLE project (
+id INTEGER PRIMARY KEY
 title TEXT,
 category TEXT,
 funding_goal TEXT,
@@ -7,5 +8,14 @@ end_date DATE
 );
 
 CREATE TABLE user (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+);
 
+CREATE TABLE pledge(
+id INTEGER PRIMARY KEY
+amount INTEGER,
+user_id INTEGER,
+project_id INTEGER
 );
